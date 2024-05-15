06dfe4cf278a7e02c4551c2ac422ccf16d711279
WuzzyLV <WuzzyLV@users.noreply.github.com>
2024-05-15 22:57:20 +0300

    Implement recipes rendering

--- src/main/java/me/wuzzyxy/showmemyrecipes/inventory/RecipeView.java
+++ src/main/java/me/wuzzyxy/showmemyrecipes/inventory/RecipeView.java
@@ -2,6 +2,7 @@ package me.wuzzyxy.showmemyrecipes.inventory;
 
 import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
 import dev.lone.itemsadder.api.FontImages.TexturedInventoryWrapper;
+import me.wuzzyxy.showmemyrecipes.RecipeManager;
 import me.wuzzyxy.showmemyrecipes.ShowMeMyRecipes;
 import net.kyori.adventure.text.Component;
 import net.md_5.bungee.api.ChatColor;
@@ -15,14 +16,17 @@ import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class RecipeView implements Listener {
     private final ShowMeMyRecipes plugin;
     private final Inventory inv;
     private final TexturedInventoryWrapper wrapper;
 
-    public RecipeView(ShowMeMyRecipes plugin) {
+    public RecipeView(ShowMeMyRecipes plugin, RecipeManager recipeManager) {
         this.plugin = plugin;
         Bukkit.getPluginManager().registerEvents(this, plugin);
 
@@ -37,6 +41,20 @@ public class RecipeView implements Listener {
         inv = wrapper.getInternal();
 
         inv.addItem(new ItemStack(Material.DIAMOND));
+
+        recipeManager.getRecipes().forEach((key, recipe) -> {
+            recipe.getRecipe();
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    ItemStack item = recipe.getRecipe()[i][j];
+                    if (item == null) continue;
+                    inv.setItem((i * 9 + j + 10) + 9, item);
+                }
+            }
+            ItemStack result = recipe.getResult();
+            inv.setItem(34, result);
+        });
 
     }
 
